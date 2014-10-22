cafef4419604f1394a4de92d800530749f410f00
Andrew White <white.d.andrew@gmail.com>
2014-10-22 03:01:09 -0500

    Completed CMake and passed first test

--- lib/grid.h
+++ lib/grid.h
@@ -1,32 +1,105 @@
 #ifndef GRID_H_
 #define GRID_H_
 
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+#include <string>
 
-template<int DIM>
+
+
+template<unsigned int DIM>
 class Grid {
-  /** A DIM-dimensional grid for storing things
+  /** A DIM-dimensional grid for storing things. Stores on 1D column-ordered array
    *
-   */
+   **/
  public:
-  Grid(double* min, double* max, int* bin_number, int* b_periodic, int* b_derivatives);
+Grid(double* min, double* max, double* bin_spacing, int* b_periodic, int b_derivatives) : b_derivatives_(b_derivatives) {
+
+    unsigned int i;
+
+    for(i = 0; i < DIM; i++) {
+      min_[i] = min[i];
+      max_[i] = max[i];
+      b_periodic_[i] = b_periodic[i];
+
+      grid_number_[i] = (int) ceil((max_[i] - min_[i]) / bin_spacing[i]);
+      dx_[i] = (max_[i] - min_[i]) / grid_number_[i];
+      //add one to grid points if 
+      grid_number_[i] = b_periodic_[i] ? grid_number_[i] : grid_number_[i] + 1;
+      //increment dx to compensate
+      if(!b_periodic_[i])
+	max_[i] += dx_[i];
+    }
+  }
+
   /** This will actually allocate the arrays and perform any sublcass initialization
    *
-   */
-  virtual void initialize();
-  double get_value(double* x);
-  int multi2one(int index[DIM]);
-  void write(std::string& filename);
-  void read(std::string& filename);
-
-  double dx[DIM];//grid spacing
-  double min[DIM];//grid minimum
-  double max[DIM];//maximum
-  int bin_number[DIM];//number of bins, equal to points in periodic and points+1 in non-periodic
-  int b_periodic[DIM];//if a dimension is periodic
-  double* grid;//the grid values
-  double* grid_deriv;//derivatives  
+   **/
+  void initialize() {
+    unsigned int i;
+    grid_size_ = 1;
+    for(i = 0; i < DIM; i++)
+      grid_size_ *= grid_number_[i];
+    grid_ = (double *) malloc(sizeof(double) * DIM * grid_size_);
+    if(b_derivatives_)
+      grid_deriv_ = (double *) malloc(sizeof(double) * DIM * grid_size_ * 3);
+  }
   
+  /**
+   * Go from a point to an array of indices
+   **/ 
+  size_t* get_index(double* x, size_t* result) {
+    unsigned int i;
+    for(i = 0; i < DIM; i++) {
+      result[i] = (int) floor((x[i] - min_[i]) / dx_[i]);
+    }
+    return result;
+  }
 
+  /**
+   * Go from an array of indices to a single index
+   **/
+  int multi2one(size_t index[DIM]) {
+    size_t result = index[DIM-1];
+
+    unsigned int i;    
+    for(i = DIM - 1; i > 0; i--) {
+      result = result * grid_number_[i-1] + index[i-1];
+    }
+    
+    return result;
+    
+  }
+
+  /**
+   * Get the value of the grid at x
+   **/ 
+  double get_value(double* x) {
+    size_t index[DIM];
+    get_index(x, index);
+    return grid_[multi2one(index)];
+  }
+   
+  void write(std::string& filename) {
+    //write grid to a file
+  }
+
+  void read(std::string& filename) {
+    //read grid from a file
+  }
+  
+  double dx_[DIM];//grid spacing
+  double min_[DIM];//grid minimum
+  double max_[DIM];//maximum
+  int grid_number_[DIM];//number of points on grid
+  long grid_size_; //total grid size
+  int b_periodic_[DIM];//if a dimension is periodic
+  int b_derivatives_;//if derivatives are going to be used
+  double* grid_;//the grid values
+  double* grid_deriv_;//derivatives  
+  
+  
 };
 
 #endif //GRID_H_
--- tests/edm_test.cpp
+++ tests/edm_test.cpp
@@ -1,29 +1,47 @@
 #include "grid.h"
 #define BOOST_TEST_DYN_LINK 
-#define BOOST_TEST_MODULE WordModel
+#define BOOST_TEST_MODULE EDM
 #include <boost/test/unit_test.hpp>
 #include <boost/timer.hpp>
 #include <sstream>
 #include <iostream>
 #include <fstream>
 #include <ctime>
+#include <cmath>
 
-using namespace wordmodel;
-using namespace std;
 using namespace boost;
 
 struct FooTest {
-  FooTests(){
+  FooTest(){
     //
   }  
 };
 
 BOOST_FIXTURE_TEST_SUITE( foo_test, FooTest )
 
-BOOST_AUTO_TEST_CASE( foo_test_1 )
+BOOST_AUTO_TEST_CASE( grid_1d_sanity )
 {
+  /* Visual:
+   * valuess:  0   1   2   3
+   * grid:   |---|---|---|---|---
+   *         0   1   2   3   4
+   */
+  double min[] = {0};
+  double max[] = {10};
+  double bin_spacing[] = {1};
+  int periodic[] = {0};
+  Grid<1> g (min, max, bin_spacing, periodic, 0);
+  g.initialize();
 
-  BOOST_REQUIRE( 2 == 2 );
+  BOOST_REQUIRE_EQUAL(g.grid_number_[0], 11);
+  BOOST_REQUIRE_EQUAL(g.grid_size_, 11);
+
+  for(int i = 0; i < 10; i++)
+    g.grid_[i] = i;
+
+  double x[] = {3.5};
+  BOOST_REQUIRE(pow(g.get_value(x) - 3, 2) < 0.000001);
+    
 }
 
 BOOST_AUTO_TEST_SUITE_END()
