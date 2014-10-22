a2720f1783536167acb0279ed2df42ae3a8c5184
Andrew White <white.d.andrew@gmail.com>
2014-10-22 09:50:59 -0500

    Added 3D tests and ability to write grid

--- lib/grid.h
+++ lib/grid.h
@@ -4,9 +4,15 @@
 #include <cmath>
 #include <cstdlib>
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <iomanip>
 
 
+inline
+int int_floor(double number) {
+  return (int) number < 0.0 ? -ceil(fabs(number)) : floor(number);                                                                   
+}                                                                                                                   
 
 template<unsigned int DIM>
 class Grid {
@@ -14,7 +20,7 @@ class Grid {
    *
    **/
  public:
-Grid(double* min, double* max, double* bin_spacing, int* b_periodic, int b_derivatives) : b_derivatives_(b_derivatives) {
+ Grid(double* min, double* max, double* bin_spacing, int* b_periodic, int b_derivatives) : b_derivatives_(b_derivatives) {
 
     unsigned int i;
 
@@ -49,9 +55,11 @@ Grid(double* min, double* max, double* bin_spacing, int* b_periodic, int b_deriv
   /**
    * Go from a point to an array of indices
    **/ 
-  size_t* get_index(double* x, size_t* result) {
+  size_t* get_index(double* x, size_t result[DIM]) {
     unsigned int i;
     for(i = 0; i < DIM; i++) {
+      if(b_periodic_[i])
+	x[i] -= (max_[i] - min_[i]) * int_floor((x[i] - min_[i]) / (max_[i] - min_[i]));
       result[i] = (int) floor((x[i] - min_[i]) / dx_[i]);
     }
     return result;
@@ -60,7 +68,7 @@ Grid(double* min, double* max, double* bin_spacing, int* b_periodic, int b_deriv
   /**
    * Go from an array of indices to a single index
    **/
-  int multi2one(size_t index[DIM]) {
+  size_t multi2one(size_t index[DIM]) {
     size_t result = index[DIM-1];
 
     unsigned int i;    
@@ -70,6 +78,19 @@ Grid(double* min, double* max, double* bin_spacing, int* b_periodic, int b_deriv
     
     return result;
     
+  }
+
+  /** 
+   * Go from single index to array
+   **/
+  void one2multi(size_t index, size_t result[DIM]) {
+    unsigned int i;
+
+    for(i = 0; i < DIM-1; i++) {
+      result[i] = index % grid_number_[i];
+      index = (index - result[i]) / grid_number_[i];
+    }
+    result[i] = index; 
   }
 
   /**
@@ -82,7 +103,56 @@ Grid(double* min, double* max, double* bin_spacing, int* b_periodic, int b_deriv
   }
    
   void write(std::string& filename) {
-    //write grid to a file
+    using namespace std;
+    ofstream output;
+    size_t i, j;
+    output.open(filename);
+
+    // print plumed-style header
+    output << "#! FORCE " << b_derivatives_ << endl;
+    output << "#! NVAR " << DIM << endl;
+    output << "#! TYPE " << 32 << endl;
+
+    output << "#! BIN ";
+    for(i = 0; i < DIM; i++)
+      output << b_periodic_[i] ? grid_number_[i] - 1 : grid_number_[i];
+    output << endl;
+
+    output << "#! MIN ";
+    for(i = 0; i < DIM; i++)
+      output << min_[i];
+    output << endl;
+
+    output << "#! MAX ";
+    for(i = 0; i < DIM; i++)
+      output << b_periodic_[i] ? max_[i] : max_[i] - dx_[i];
+    output << endl;
+
+    output << "#! PBC ";
+    for(i = 0; i < DIM; i++)
+      output << b_periodic_[i];
+    output << endl;
+
+
+    //print out grid
+    double point;
+    size_t temp[DIM];
+    for(i = 0; i < grid_size_; i++) {
+      one2multi(i, temp);
+      for(j = 0; j < DIM; j++) {
+	output << setw(8) << (min_[j] + dx_[j] * temp[j]) << " ";
+      }
+      output << setw(8) << grid_[i] << " ";
+      if(b_derivatives_) {
+	for(j = 0; j < 3; j++) {
+	  output << setw(8) << grid_deriv_[i*3 + j] << " ";
+	}
+      }
+      output << endl;
+      if(temp[0] == grid_number[0] - 1)
+	output << endl;
+    }
+
   }
 
   void read(std::string& filename) {
--- tests/edm_test.cpp
+++ tests/edm_test.cpp
@@ -9,6 +9,8 @@
 #include <ctime>
 #include <cmath>
 
+#define EPSILON 1e-5
+
 using namespace boost;
 
 struct FooTest {
@@ -36,6 +38,11 @@ BOOST_AUTO_TEST_CASE( grid_1d_sanity )
   BOOST_REQUIRE_EQUAL(g.grid_number_[0], 11);
   BOOST_REQUIRE_EQUAL(g.grid_size_, 11);
 
+  size_t array[] = {5};
+  size_t temp[1];
+  g.one2multi(g.multi2one(array), temp);
+  BOOST_REQUIRE_EQUAL(array[0], temp[0]);
+
   for(int i = 0; i < 10; i++)
     g.grid_[i] = i;
 
@@ -43,5 +50,57 @@ BOOST_AUTO_TEST_CASE( grid_1d_sanity )
   BOOST_REQUIRE(pow(g.get_value(x) - 3, 2) < 0.000001);
     
 }
+
+BOOST_AUTO_TEST_CASE( grid_3d_sanity )
+{
+  double min[] = {-2, -5, -3};
+  double max[] = {125, 63, 78};
+  double bin_spacing[] = {1.27, 1.36, 0.643};
+  int periodic[] = {0, 1, 1};
+  Grid<3> g (min, max, bin_spacing, periodic, 0);
+  g.initialize();
+
+  BOOST_REQUIRE_EQUAL(g.grid_number_[0], 101);
+  BOOST_REQUIRE_EQUAL(g.grid_number_[1], 50);
+  BOOST_REQUIRE_EQUAL(g.grid_number_[2], 126);
+
+  size_t array[3];
+  size_t temp[3];
+  for(int i = 0; i < g.grid_number_[0]; i++) {
+    array[0] = i;
+    for(int j = 0; j < g.grid_number_[1]; j++) {
+      array[1] = j;
+      for(int k = 0; k < g.grid_number_[2]; k++) {
+	array[2] = k;
+
+	//check to make sure the index conversion is correct
+	g.one2multi(g.multi2one(array), temp);
+	BOOST_REQUIRE_EQUAL(array[0], temp[0]);
+	BOOST_REQUIRE_EQUAL(array[1], temp[1]);
+	BOOST_REQUIRE_EQUAL(array[2], temp[2]);
+
+	g.grid_[g.multi2one(array)] = g.multi2one(array);
+      }
+    }
+  }
+
+  double point[3];
+  for(int i = 0; i < g.grid_number_[0]; i++) {
+    point[0] = i * g.dx_[0] + g.min_[0] + EPSILON;
+    array[0] = i;
+    for(int j = 0; j < g.grid_number_[1]; j++) {
+      point[1] = j * g.dx_[1] + g.min_[1] + EPSILON;
+      array[1] = j;
+      for(int k = 0; k < g.grid_number_[2]; k++) {
+	point[2] = k * g.dx_[2] + g.min_[2] + EPSILON;
+	array[2] = k;
+	BOOST_REQUIRE(pow(g.get_value(point) - g.multi2one(array),2) < 0.0000001);
+      }
+    }
+  }  
+}
+
+
+
 
 BOOST_AUTO_TEST_SUITE_END()
